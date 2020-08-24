bf6c1be1d0559cd5f7bb8208f0737a93ff9759f5
swaraj <swarajdeshmukh45@gmail.com>
2020-08-24 22:39:54 +0530

    commit

---
+++ src/Array/SmallestMissingPositive.java
@@ -0,0 +1,23 @@
+package Array;
+
+import java.util.Arrays;
+
+public class SmallestMissingPositive {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3,4,0,1,2};
+        //output = 2
+        smallestmissingpositive(arr);
+
+    }
+
+    private static int smallestmissingpositive(int[] arr) {
+
+        Arrays.sort(arr);
+        for (int i = )
+
+
+        return 0;
+    }
+}
