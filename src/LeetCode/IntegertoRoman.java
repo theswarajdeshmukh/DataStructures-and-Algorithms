4d71e40af548f9a33895e04289317df192c60590
swaraj <swarajdeshmukh45@gmail.com>
2020-09-12 12:44:42 +0530

    IntegertoRoman, longestPalindromesubstring

---
+++ src/LeetCode/IntegertoRoman.java
@@ -0,0 +1,23 @@
+package LeetCode;
+
+public class IntegertoRoman {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+        System.out.println(InttoRoman(num));
+    }
+
+    public static String InttoRoman(int num){
+        String[] units = {"" , "I", "II", "III","IV","V","VI","VII","VIII","IX"};
+        String[] tens = {"" , "X", "XX", "XXX","XL","L","LX","LXX","LXXX","XC"};
+        String[] hundreds = {"" , "C", "CC", "CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] thousands = new String[]{"","M","MM","MMM"};
+
+        return thousands[num /1000] +
+                hundreds[(num % 1000) / 100] +
+                tens[(num % 100) / 10] +
+                units [num % 10];
+
+    }
+}
